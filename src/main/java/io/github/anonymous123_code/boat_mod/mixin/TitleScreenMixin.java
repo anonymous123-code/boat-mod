f631ecd690bf4ee3b79430e6f1fa1c88a64cc572
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-08-13 10:03:42 +0200

    Initial setup & update icon.png

--- src/main/java/io/github/anonymous123_code/boat_mod/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/anonymous123_code/boat_mod/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package io.github.anonymous123_code.boat_mod.mixin;
 
-import com.example.example_mod.ExampleMod;
+import io.github.anonymous123_code.boat_mod.BoatMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		BoatMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
